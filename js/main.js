c2db158e457d74a3cb29bbcd0ef697ae569c83bb
antonio_tomas\Usuario <antoniotomassalinas@gmail.com>
2023-06-04 19:30:20 +0200

    nueva tarea implementada por consola. Hay que almacenarla en array

--- js/main.js
+++ js/main.js
@@ -1,17 +1,34 @@
-const taskList = [
-    {
-        idTarea: 1,
-        titulo: 'Estudiar JavaScript',
-        prioridad: 'urgente'
-    },
-    {
-        idTarea: 2,
-        titulo: 'Dormir',
-        prioridad: 'diaria'
-    },
-    {
-        idTarea: 3,
-        titulo: 'Salir a comer',
-        prioridad: 'mensual'
-    },
-]
+const guardarTarea = document.querySelector('#guardarTarea');
+const selectPrioridad = document.querySelector('#selectPrioridad');
+const inputGuardar = document.querySelector('#inputGuardar');
+const btnGuardar = document.querySelector('#guardar')
+let id = 3;
+
+guardarTarea.addEventListener('submit', getData);
+
+/* function saveTask(pList, pTask) {
+    let duplicado = pList.findIndex(task => task.titulo === pTask.titulo);
+    if (duplicado === -1) {
+        //puedo hacer push
+        pList.push(pTask);
+        return 'Tarea guardada';
+    }
+    //no puedo hacer push
+    return 'Tarea duplicada';
+} */
+
+
+
+function getData(event) {
+    event.preventDefault();
+
+    const newTask = {
+        id: id,
+        titulo: event.target.titulo.value,
+        prioridad: event.target.prioridad.value
+    }
+
+    /* let guardado = saveTask(taskList, newTask) */
+
+    console.log(newTask)
+}
