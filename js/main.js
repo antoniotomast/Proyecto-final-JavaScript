c9ca64cbe41218e262a7dc1d535065f052590004
antonio_tomas\Usuario <antoniotomassalinas@gmail.com>
2023-06-06 19:51:54 +0200

    Pintar las tareas OK

--- js/main.js
+++ js/main.js
@@ -1,12 +1,14 @@
 const guardarTarea = document.querySelector('#guardarTarea');
 const selectPrioridad = document.querySelector('#selectPrioridad');
 const inputGuardar = document.querySelector('#inputGuardar');
-const btnGuardar = document.querySelector('#guardar')
+const btnGuardar = document.querySelector('#guardar');
+const ul = document.querySelector('main ul');
+
 let id = 3;
 
 guardarTarea.addEventListener('submit', getData);
 
-/* function saveTask(pList, pTask) {
+function saveTask(pList, pTask) {
     let duplicado = pList.findIndex(task => task.titulo === pTask.titulo);
     if (duplicado === -1) {
         //puedo hacer push
@@ -15,8 +17,12 @@ guardarTarea.addEventListener('submit', getData);
     }
     //no puedo hacer push
     return 'Tarea duplicada';
-} */
+}
 
+
+function comprobarForm(pForm) {
+    return pForm.titulo.value !== "";
+}
 
 
 function getData(event) {
@@ -28,7 +34,30 @@ function getData(event) {
         prioridad: event.target.prioridad.value
     }
 
-    /* let guardado = saveTask(taskList, newTask) */
+    let guardado = saveTask(taskList, newTask)
 
-    console.log(newTask)
+    if (guardado !== null) {
+        printOneTask(newTask, ul);
+    } else {
+        alert('No se ha podido crear la tarea. Inténtalo de nuevo');
+    }
 }
+
+
+function printOneTask(pTask, pDom) {
+    const li = document.createElement('li');
+    const button = document.createElement('button');
+    li.classList.add(`${pTask.prioridad}`);
+
+    li.textContent = pTask.titulo;
+    button.textContent = 'Borrar';
+    pDom.append(li);
+    li.append(button);
+}
+
+
+/*<ul id="listaTareas">
+    <li><button class="delete"></button></li>
+    <li><button class="delete"></button></li>
+    <li><button class="delete"></button></li>
+</ul> */
