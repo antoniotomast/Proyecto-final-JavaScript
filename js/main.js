59707bacdb8e840807943d9147d4d30e7ae6644d
antonio_tomas\Usuario <antoniotomassalinas@gmail.com>
2023-06-09 18:26:06 +0200

    Botón Enter implementado

--- js/main.js
+++ js/main.js
@@ -4,11 +4,13 @@ const inputGuardar = document.querySelector('#inputGuardar');
 const btnGuardar = document.querySelector('#guardar');
 const ul = document.querySelector('main ul');
 const inputSearch = document.querySelector('#search');
+const filtro = document.querySelector('#filtro');
 
 let id = 3;
 
 guardarTarea.addEventListener('submit', getData);
 inputSearch.addEventListener('keypress', getSearch);
+filtro.addEventListener('change', getPriority);
 
 function saveTask(pList, pTask) {
     let duplicado = pList.findIndex(task => task.titulo === pTask.titulo);
@@ -64,21 +66,70 @@ function printOneTask(pTask, pDom) {
 
 }
 
-function getSearch(event) {
-    event.key === 'enter';
-
-}
-
-
 function deleteElement(event) {
     event.target.parentElement.remove();
 }
 
-function filterByPriority(pList, pPrioridad) {
-    return pList.filter(task => task.prioridad.toLowerCase() === pPrioridad.toLowerCase());
+
+function getSearch(event) {
+    let word = '';
+    if (event.target.id === 'search') {
+        if (event.key = 'Enter') {
+            word = event.target.value;
+            const listFiltered = filterByWord(taskList, word);
+            printAllTasks(listFiltered, ul);
+        }
+    }
+
 }
 
+function filterByWord(pList, pWord) {
+    return pList.filter(task => task.titulo.toLowerCase().includes(pWord.toLowerCase()));
+}
+
+const filtrarPalabra = filterByWord(taskList, task.titulo)
+
+function getName(event) {
+    let palabraBuscar = event.target.value;
+    let listaFiltrada = filterByWord(taskList, palabraBuscar);
+    printTasks(listaFiltrada, ul);
+}
+
+function filterByPriority(pList, pPrioridad) {
+
+    const listaFiltrada = [];
+
+    for (let task of pList) {
+        if (task.prioridad.toLowerCase() === pPrioridad.toLowerCase()) {
+            listaFiltrada[listaFiltrada.length] = task;
+        }
+    }
+
+    return listaFiltrada;
+}
+
+function getPriority(event) {
+    let listaFiltrada = filterByPriority(taskList, event.target.value);
+    printTasks(listaFiltrada, ul);
+
+}
+
+
+
+function printTasks(pList, pDom) {
+    /* pList.forEach(task => printOneTask(task, pDom)); */
+    pDom.innerHTML = '';
+    if (pList.length !== 0) {
+        pList.forEach(task => printOneTask(task, pDom));
+    } else {
+        pDom.innerHTML = '<h2>NO HAY RESULTADOS</h2>'
+    }
+}
+
+printTasks(taskList, ul);
+
 function printAllTasks(pList, pDom) {
+    pDom.innerHTML = ''
     pList.forEach(task => printOneTask(task, pDom));
 }
 
